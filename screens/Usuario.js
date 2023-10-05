3a27ed0bf524bd8ddbdf56d54a4dc16b16fa62d7
danacaro <dana.caro.ram@gmail.com>
2023-10-05 11:41:45 -0600

    tarea1

---
+++ screens/Usuario.js
@@ -0,0 +1,31 @@
+import { Text, View, Image } from 'react-native'
+import React, { Component } from 'react'
+
+export default class Usuario extends Component {
+    //static contextType = NavigationContext;
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+    }
+
+    render() {
+        
+        return (
+          <View>
+            
+            <Text style={{marginTop:15, color: 'pink', fontSize: 20, textAlign: 'center'}}> Nombre: <Text style={{color:'black'}}>{this.props.route.params.Nombre}</Text></Text>
+            <Text style={{color: 'pink', fontSize: 20, textAlign: 'center',}}> Profesion: <Text style={{color:'black'}}>{this.props.route.params.Profesion}</Text></Text>
+            <Text style={{color: 'pink', fontSize: 20, textAlign: 'center',}}> Telefono: <Text style={{color:'black'}}></Text> this.props.route.params.Telefono}</Text>
+            <Image
+            source={{uri: this.props.route.params.Imagen}}
+            style={{width: 100, height:100,marginTop:15,marginBottom:15, marginLeft:'40%'}}
+                
+            />
+
+
+          </View>
+        );
+      }
+    }
